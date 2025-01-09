42d6cfffe93a5d45c6ef1f10cd701b4fa4cf5d43
wasonchueng <wahsonzhang@gmail.com>
2025-01-09 18:01:47 +0800

    权限权限逻辑

--- app/Foundation/Entities/PermissionDO.php
+++ app/Foundation/Entities/PermissionDO.php
@@ -14,6 +14,18 @@ class PermissionDO
 
     private array $permissions;
 
+    private ?string $categoryLabel = null;
+
+    private ?string $groupLabel = null;
+
+    private array $permissionsLabel = [];
+
+    private mixed $categoryLabelUsing = null;
+
+    private mixed $groupLabelUsing = null;
+
+    private mixed $permissionsLabelUsing = null;
+
     public function __construct(public DefaultGuard $guard) {}
 
     public static function make(DefaultGuard $guard): PermissionDO
@@ -55,5 +67,41 @@ class PermissionDO
     public function getCategory(): string
     {
         return $this->category;
+    }
+
+    public function categoryLabel(?string $categoryLabel): self
+    {
+        $this->categoryLabel = $categoryLabel;
+        return $this;
+    }
+
+    public function permissionsLabelUsing(mixed $permissionsLabelUsing): self
+    {
+        $this->permissionsLabelUsing = $permissionsLabelUsing;
+        return $this;
+    }
+
+    public function groupLabelUsing(mixed $groupLabelUsing): self
+    {
+        $this->groupLabelUsing = $groupLabelUsing;
+        return $this;
+    }
+
+    public function categoryLabelUsing(mixed $categoryLabelUsing): self
+    {
+        $this->categoryLabelUsing = $categoryLabelUsing;
+        return $this;
+    }
+
+    public function permissionsLabel(array $permissionsLabel): self
+    {
+        $this->permissionsLabel = $permissionsLabel;
+        return $this;
+    }
+
+    public function groupLabel(?string $groupLabel): self
+    {
+        $this->groupLabel = $groupLabel;
+        return $this;
     }
 }
